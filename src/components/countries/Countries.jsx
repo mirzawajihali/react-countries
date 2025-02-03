06523b6dc06537d0f48a2c6f9ae204854a8cdea4
mirzawajihali <mirzawajihali00@gmail.com>
2025-02-03 18:04:02 +0600

    aybe the last commit

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -2,10 +2,20 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../country/Country";
+import "./Countries.css"
 
 const countries = () => {
 
     const [countries, setCountries] = useState([]);
+
+    const [visitedCountries , setVisitedCountries] = useState([]);
+
+
+    const handleVisitedCountry = country => {
+        const newVisitedCountries = [...visitedCountries, country];
+
+        setVisitedCountries(newVisitedCountries)
+    }
 
     useEffect(() => {
         fetch( `https://restcountries.com/v3.1/all`)
@@ -14,14 +24,29 @@ const countries = () => {
     },[])
 
     return (
-        <div>
+        <div >
+
+            
 
             <h5>countries : {countries.length}</h5>
 
-            {
-                countries.map(country=><Country key ={country.cca3} country={country}></Country> 
+            <div>
+                <h5>visited Countries : {visitedCountries.length}</h5>
+                <ul>
+                    
+                {
+                    visitedCountries.map(country =><li key={country.cca3} >{country.name.common}</li>
+                    )
+                }
+                </ul>
+            </div>
+
+         <div className="country-container">
+         {
+                countries.map(country=><Country key ={country.cca3} country={country} handleVisitedCountry ={handleVisitedCountry}></Country> 
                 )
             }
+         </div>
             
         </div>
     );
